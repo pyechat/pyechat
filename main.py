e2d58f0deb255f0bd10f68017559d66e6544c80f
ComputerTech312 <computertech312@gmail.com>
2024-02-14 08:45:05 +0100

    Modified code from PyQt5 to PyQt6 and cleaned up the code.

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from PyQt5.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QDialog
 from src.irc.irc_client import IRCClient
 from src.gui.connect_dialog import ConnectDialog
 from src.gui.main_window import MainWindow
 import sys
-from PyQt5.QtWidgets import QApplication, QDialog
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     dialog = ConnectDialog()
-    if dialog.exec() == QDialog.Accepted:
+    if dialog.exec() == QDialog.DialogCode.Accepted:
         host, port, nick, realname, channel = dialog.get_values()
         client = IRCClient(host, port, nick, realname, channel)
         client.connect_to_host()
